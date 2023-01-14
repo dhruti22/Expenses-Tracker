885a73340f69b699257e9e74bf5a82d3960e1ccf
dhruti maheta <dhrutimehta2001@gmail.com>
2023-01-14 08:09:48 -0800

    project

---
+++ MVC_Project_1/Models/Expense.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Project_1.Models
+{
+    [Table("tbl_Expense")]
+    public class Expense
+    {
+        [Key]
+        public int Exp_Id { get; set; }
+
+        [Required(ErrorMessage ="*Title Required")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "*Description Required")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "*Amount Required")]
+        public long Amount{ get; set; }
+
+        [Display(Name ="tbl_Category")]
+        [Required(ErrorMessage ="Select Any Category,If Null Then first Add Category")]
+        public virtual int Id { get; set; }
+
+        [ForeignKey("Id")]
+        public virtual ExCategory ExCat { get; set; }
+
+        [Required(ErrorMessage ="Date Required")]
+        public DateTime Date { get; set; }
+    }
+}
---
+++ MVC_Project_1/Models/MultiViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Project_1.Models
+{
+    public class MultiViewModel
+    {
+        public List<ExCategory> exCategories = new List<ExCategory>();
+
+        public List<Expense> Expenses = new List<Expense>();
+
+        public List<totalLimit> totalLimits = new List<totalLimit>();
+
+    }
+}
